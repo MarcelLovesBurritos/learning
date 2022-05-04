25f6f36fd1d0563f4d751265469474ed8822d21f
Evgeny Dashutin <eugenedashutin@yahoo.com>
2022-05-04 23:34:07 +0400

    Added Insertion&Selections sortings

--- Sorting.java
+++ Sorting.java
@@ -1,15 +1,18 @@
 /*Sorting
  * 1. Bubble
  * 2. Cocktail
- * 3. 
- * 4. 
- * 5. */
+ * 3. Insertion
+ * 4. Selection
+ * 5. 
+ * 6. 
+ * 7. 
+ * 8. */
 
 import java.util.*;
 
 public class Sorting {
 	
-	public void bubbleSort (int[] array){
+	public void bubbleSort (int[] array){ // 1. Bubble
 		int temp;
 		boolean sorted = false;
 		while (sorted == false){
@@ -26,7 +29,7 @@ public class Sorting {
 		System.out.println ("Array sorted by Bubble: " + Arrays.toString(array));
 	}
 	
-	public void cocktailSort (int[] array){
+	public void cocktailSort (int[] array){ // 2. Cocktail
 		int temp;
 		int left = 0;
 		int right = array.length - 1;
@@ -49,14 +52,47 @@ public class Sorting {
 			left++;
 		}
 		System.out.println ("Array sorted by Cocktail: " + Arrays.toString(array));
-	}	
+	}
+	
+	public void insertionSort (int[] array){ // 3. Insertion
+		for (int i = 1; i < array.length; i++){
+			int temp = array[i];
+			int j = i - 1;
+			while (j >= 0 && temp < array[j]){
+				array[j+1] = array[j];
+				j--;
+			}
+			array[j+1] = temp;
+		}
+		System.out.println ("Array sorted by Insertion: " + Arrays.toString(array));
+	}
+	
+	public void selectionSort (int[] array){ // 4. Selection
+		for (int i = 0; i < array.length; i++){
+			int min = array[i];
+			int minN = i;
+			int temp;
+			for (int j = i + 1; j < array.length; j++){
+				if (array[j] < min){
+					min = array[j];
+					minN = j;
+				}
+			}
+			temp = array[i];
+			array[i] = min;
+			array[minN] = temp;
+		}
+		System.out.println ("Array sorted by Selection: " + Arrays.toString(array));
+	}
 	
 	
 	public static void main (String[] args) {
 		int[] array = {4, 8, 3, 7, 5, 9, 0, 1, 2, 6};
 		Sorting sorting = new Sorting();
 		//sorting.bubbleSort(array);
-		sorting.cocktailSort(array);
+		//sorting.cocktailSort(array);
+		//sorting.insertionSort(array);
+		sorting.selectionSort(array);
 	}
 }
 
